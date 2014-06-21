083287e6a1149d55798db8195021409cb02dd0f4
Ruud Wijtvliet <rwijtvliet@gmail.com>
2014-06-21 02:56:17 +0200

    Data validation for e.g. NaN values

--- js/convert.js
+++ js/convert.js
@@ -7,8 +7,10 @@
 (function(){
     var d = d3.select("#inf");
 
-    d3.xml("data/nrg_100a_A.TJ.0000.B_100100......_ALL_startPeriod=2005_endPeriod=2009.xml", function(error, xml){
-         //http://ec.europa.eu/eurostat/SDMX/diss-web/rest/data/nrg_100a/A.TJ.0000.B_100100......?startPeriod=2005_endPeriod=2009.xml
+    d3.xml("data/nrg_100a_A.TJ.0000+2000+3000.B_100100+B_101847+B_101321.._ALL.xml", function(error, xml){
+        //"http://ec.europa.eu/eurostat/SDMX/diss-web/rest/data/nrg_100a/A.TJ.0000+2000+3000...?startPeriod=2005&endPeriod=2006"
+        //"http://ec.europa.eu/eurostat/SDMX/diss-web/rest/data/nrg_100a/A.TJ.0000.B_100100..?startPeriod=2005&endPeriod=2009"
+        //"http://ec.europa.eu/eurostat/SDMX/diss-web/rest/data/nrg_100a/A.TJ.0000+2000+3000.B_100100.."
 
         if (error) d.append("p").text(error);
 
@@ -25,14 +27,19 @@
         data.forEach(function(d){
             //get all data that remains the same (=unit, country, product, indic_nrg, ...)
             var v_base = {};
-            d.SeriesKey.Value.forEach(function(v){
-                v_base[v["_id"]] = v["_value"];
-            });
-            d.Obs.forEach(function(o){ 
+            d["SeriesKey"]["Value"].forEach(function(skv){ v_base[skv["_id"]] = skv["_value"]; });
+            d["Obs"].forEach(function(o){
                 var v = {};
                 $.extend(v,v_base);
-                v["YEAR"] = o.ObsDimension["_value"];
-                v["VALUE"] = o.ObsValue["_value"];
+                v["YEAR"] = Number(o.ObsDimension["_value"]);
+                if ("Attributes" in o && "Value" in o["Attributes"] && "_id" in o["Attributes"]["Value"] && o["Attributes"]["Value"]["_id"] === "OBS_STATUS") {
+                    v["VALUE"] = (o["Attributes"]["Value"]["_value"] === "na" ? null : 0); //value unknown or (practically) zero.
+                } else {
+                    v["VALUE"] = o.ObsValue["_value"];
+                    if (!isNaN(v["VALUE"])){ //on this value there might be a flag. If so, it should be tested which one and what it means (TODO)
+                        v["VALUE"] = Number(v["VALUE"]);
+                    }
+                }
                 newdata.push(v);
             });
         });
