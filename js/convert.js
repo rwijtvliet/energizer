5f53e0fc57f05946082d99544443ada32188a642
Ruud Wijtvliet <rwijtvliet@gmail.com>
2014-06-26 01:00:46 +0200

    Scalable d3 graph

--- js/convert.js
+++ js/convert.js
@@ -4,17 +4,24 @@
 
 
 
-(function(){
-    var div = d3.select("#inf");
+(function () {
+    var margin = {top: 10, right: 20, bottom: 30, left: 90},
+        graphSize = [1000 - margin.left - margin.right, 700 - margin.top - margin.bottom];
 
-    d3.xml("data/nrg_100a_A.TJ.0000.B_100100.DE._ALL_startPeriod=1990_endPeriod=2020.xml" , function(error, xml){
+    var svg = d3.select("#inf").append("svg")
+        .attr("width", graphSize[0] + margin.left + margin.right)
+        .attr("height", graphSize[1] + margin.top + margin.bottom);
+
+
+    d3.xml("data/nrg_100a_A.TJ.0000.B_100100.DE._ALL_startPeriod=1990_endPeriod=2020.xml" , function (error, xml) {
         //"http://ec.europa.eu/eurostat/SDMX/diss-web/rest/data/nrg_100a/A.TJ.0000+2000+3000...?startPeriod=1980&endPeriod=2006"//very large file
         //"http://ec.europa.eu/eurostat/SDMX/diss-web/rest/data/nrg_100a/A.TJ.0000.B_100100.DE.?startPeriod=1990&endPeriod=2020"
         //"http://ec.europa.eu/eurostat/SDMX/diss-web/rest/data/nrg_100a/A.TJ.0000+2000+3000.B_100100.."
-
+        var div = d3.select("#inf");
 
         try { //Basic initial data validation.
             if (error) throw error;
+
             var converter = new X2JS();
             var alldata = converter.xml2json(xml);
             if (!("GenericData" in alldata)) throw "Unexpected xml document; node 'GenericData' not found";
@@ -58,35 +65,77 @@
         div.append("h2").text("- data in database table style; after parsing");
         div.append("p").text(JSON.stringify(newdata));
 
+        makeGraph(newdata);
 
-        /*//using xml2json.js -- the worse option
-        data = xml2json(xml);
-        div.append("h1").text("using xml2json");
-        div.append("h3").text("- data as present in xml file");
-        div.append("p").text(JSON.stringify(data));*/
+    });
 
-        var svgSize = [500,300]
-        var svg = d3.select("#inf").append("svg")
-            .attr("width", svgSize[0])
-            .attr("height", svgSize[1]);
+    function makeGraph(data) {
+        var graph = svg.append("g")
+            .attr("width", graphSize[0])
+            .attr("height", graphSize[1])
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        var scaleY = d3.scale.linear()
-            .domain([0, d3.max(newdata, function(d){return d.VALUE;})])
-            .range([0, svgSize[1]]);
+        var yDomain = [
+                Math.min(0, d3.min(data, function (d) {return d.VALUE;})),
+                Math.max(0, d3.max(data, function (d) {return d.VALUE;}))
+            ],
+            yRange = [graphSize[1], 0],
+            xDomain = [
+                d3.min(data, function (d) {return d.YEAR;})-0.5,
+                d3.max(data, function (d) {return d.YEAR;})+0.5
+            ],
+            xRange = [0, graphSize[0]],
+            xScale = d3.scale.linear()
+                .domain(xDomain)
+                .range(xRange),
+            yScale = d3.scale.linear()
+                .domain(yDomain)
+                .range(yRange),
+            yColorScale = d3.scale.linear()
+                .domain(yDomain)
+                .range(["green", "orange"]),
+            xAxis = d3.svg.axis()
+                .scale(xScale)
+                .orient("bottom")
+                .tickFormat(d3.format("0000")),
+            yAxis = d3.svg.axis()
+                .scale(yScale)
+                .orient("left");
 
-        var scaleY2 = d3.scale.linear()
-            .domain([0 , d3.max(newdata, function(d){return d.VALUE;})])
-            .range(["green", "orange"]);
-
-        svg.selectAll("rect")
-            .data(newdata)
+        //Add bars
+        graph.append("g")
+            .attr("class", "bars")
+          .selectAll(".bar")
+            .data(data)
           .enter()
           .append("rect")
-            .attr("x", function(d){return (d.YEAR -1990)*20;})
-            .attr("y", function(d){return svgSize[1]-scaleY(d.VALUE);})
-            .attr("height", function(d){return scaleY(d.VALUE);})
-            .attr("width", 15)
-            .attr("style", function(d){return "fill:" + scaleY2(d.VALUE);});
-    });
+            .attr("class", "bar")
+            .attr("x", function(d){return xScale(d.YEAR)-15;})
+            .attr("y", function(d){return yScale(d.VALUE);})
+            .attr("height", function(d){return graphSize[1] - yScale(d.VALUE);})
+            .attr("width", 30);
+            //.attr("style", function(d){return "fill:" + yColorScale(d.VALUE);});
+
+        //Add axes
+        graph.append("g") //axis itself
+            .attr("class", "axis")
+            .attr("transform", "translate(0," + graphSize[1] + ")")
+            .call(xAxis)
+          .append("text") //label
+            .attr("y", -4)
+            .attr("x", graphSize[0])
+            .style("text-anchor", "end")
+            .text("Year");
+        graph.append("g") //axis itself
+            .attr("class", "axis")
+            .call(yAxis)
+          .append("text") //label
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("\u00D7 1000000 TJ");
+
+    }
 
 })();
