a6a8e30ec6f4bdb68167dcdc18803f1df8dba4e0
Ruud Wijtvliet <rwijtvliet@gmail.com>
2014-08-17 23:32:11 +0200

    small optimisations

--- js/main.js
+++ js/main.js
@@ -50,15 +50,8 @@ app.controller("BarChartController", ["$scope", "$window", "PrepareTable", "Fetc
         .then(function(tbl){
             $scope.products = db.codelist(tbl.name, "PRODUCT");
             $scope.geos = db.codelist(tbl.name, "GEO");
-        });
-
-    /*tblQ["nrg_100a"].then(getCodelist("geos", "GEO")).then(getCodelist("products", "PRODUCT"));
-    function getCodelist(target, fldName) {
-        return function (tbl) {
-            $scope[target] = db.codelist(tbl.name, fldName);
-            return tbl;
-        };
-    }*/
+        })
+        .catch(showError);
 
     $scope.updateGraph = function () {
         FetchRst(tblQ["nrg_100a"], $scope.fieldFilter)
@@ -90,18 +83,21 @@ app.directive('barChart', function() {
             xScale = d3.scale.linear()//not using d3.time.scale because always entire years plotted.
                 .rangeRound(xRange),
             xAxis = d3.svg.axis()
+                .scale(xScale)//can change xScale without needing to reapply it here.
                 .orient("bottom")
                 .tickFormat(d3.format("0000")),
             xAxisEl = graph.append("g") //axis
                 .attr("class", "axis"),
             xAxisLabel = xAxisEl.append("text") //label
                 .attr("y", -4)
+                .attr("x", graphSize[0])
                 .style("text-anchor", "end")
                 .text("Year"),
             yRange = [graphSize[1], 0],
             yScale = d3.scale.linear()
                 .rangeRound(yRange).nice(),
             yAxis = d3.svg.axis()
+                .scale(yScale)
                 .orient("left")
                 .tickFormat(d3.format("s")),//function(d){return d/1e6;}
             yAxisEl = graph.append("g") //axis
@@ -111,7 +107,14 @@ app.directive('barChart', function() {
                 .attr("y", 6)
                 .attr("dy", ".71em")
                 .style("text-anchor", "end")
-                .text("TJ/a"); //"\u00D7 1000000 TJ/a"
+                .text("TJ/a"), //"\u00D7 1000000 TJ/a"
+            rect = {
+                top: function(d) {if (d.OBS_VALUE >= 0) return yScale(d.OBS_VALUE); else return yScale(0);},
+                height: function(d) {return Math.abs(yScale(0) - yScale(d.OBS_VALUE));},
+                left: function(d) {return xScale(d.TIME - 0.5 * xBarFillRatio);},
+                width: function (d) {return xScale(d.TIME + 0.5 * xBarFillRatio) - xScale(d.TIME - 0.5 * xBarFillRatio);}
+            };
+
 
         //Update things on resize.
         scope.$watch(function () {
@@ -124,16 +127,17 @@ app.directive('barChart', function() {
         scope.$watch("data", function (data) {//data = recordset
             if (!data || !data.length) return;
 
-            //DEBUG:
-            var remove = Math.floor(Math.random()*5);
+            /*//DEBUG:
+            var remove = Math.floor(Math.random()*3);
             for (var x=0; x<remove;x++) data.pop();
-            remove = Math.floor(Math.random()*5);
+            remove = Math.floor(Math.random()*3);
             for (x=0; x<remove;x++) data.shift();
+            data.splice(Math.floor(Math.random()*(data.length-3)),1);
             if (!data || !data.length) return;
-            //
+            //*/
 
             var duration = 1000,
-                ease = "cubic-in-out", //"cubic-in-out" or "elastic"
+                ease = "cubic-in-out", //"cubic-in-out" or "elastic" er d3.ease("elastic", valA, valP)
                 yDomain = [
                     Math.min(0, d3.min(data, function (d) {return d.OBS_VALUE;})),
                     Math.max(0, d3.max(data, function (d) {return d.OBS_VALUE;}))
@@ -144,104 +148,56 @@ app.directive('barChart', function() {
                 ];
 
             if (yDomain[0]===yDomain[1]) {yDomain[0] = -1;yDomain[1] = 1;}
-            xScale.domain(xDomain);
-            yScale.domain(yDomain);
 
-            /*yScale.transition()
-                .duration(duration)
-                .domain(yDomain);//*/
-            xAxis.scale(xScale);//TODO:necessary? or can go to top?
-            xAxisLabel.attr("x", graphSize[0]);
-            yAxis.scale(yScale);//TODO:necessary? or can go to top?
-
-            var rect = {
-                    top: function(d) {if (d.OBS_VALUE >= 0) return yScale(d.OBS_VALUE); else return yScale(0);},
-                    height: function(d) {return Math.abs(yScale(0) - yScale(d.OBS_VALUE));},
-                    left: function(d) {return xScale(d.TIME - 0.5 * xBarFillRatio);},
-                    width: function (d) {return xScale(d.TIME + 0.5 * xBarFillRatio) - xScale(d.TIME - 0.5 * xBarFillRatio);}
-                };
-
-            //see http://bl.ocks.org/enjalot/1429426 for example
-
-
-            //Transition 1: new scale on Y
-
-
-            //new data
-            bars = graph.select("#bars")
+            //Add data before new domains are put into effect.
+            /*bars = graph.select("#bars")
                 .selectAll(".bar")
-                .data(data, function(d){return d.TIME;}); //the 1994 element is updated to also represent 1994 after transition.
-            //bars = bars.data(data, function(d){return d.TIME;}); //somehow not working if multiple products selected.
+                .data(data, Math.random); //all new data in .enter(), all old data in .exit()*/
+            bars = bars.data(data, Math.random); //somehow not working if multiple products selected.
 
-
-            //update
-            bars.attr("class", function(d){return "bar pr" + d.PRODUCT;})
-                //.attr("class", function(d){var cl = "bar pr" + d.PRODUCT; if (d.OBS_VALUE < 0) cl += " neg"; return cl;});
-                .classed("neg", function(d){return (d.OBS_VALUE < 0);});
-
-            //enter
-            bars.enter()
-              .append("rect")
-                .attr("x", rect.left)
-                .attr("y", yScale(0))
-                .attr("height", 0)
-                .attr("width", rect.width)
-              .append("svg:title")
-                .text(function (d) {return d.GEO + "     " + d.TIME + "\nProduct: " + d.PRODUCT + "\n" + d.OBS_VALUE + " TJ/a";});
-
-            //exit
-            bars.exit()
-              .transition()
-                .duration(duration)
-                .style("opacity", 0)
-                .remove();
-
-            //update + enter
-            bars.transition()
-                .duration(duration)
-                .ease(ease)
+            bars.enter().append("rect") //starting positions
                 .attr("class", function(d){var cl = "bar pr" + d.PRODUCT; if (d.OBS_VALUE < 0) cl += " neg"; return cl;}) //'classed' not working in transition for some reason
                 .attr("x", rect.left)
                 .attr("y", rect.top)
                 .attr("height", rect.height)
-                .attr("width", rect.width);
+                .attr("width", rect.width)
+                .style("opacity", 1)
+              .append("svg:title")
+                .text(function (d) {return d.GEO + "     " + d.TIME + "\nProduct: " + d.PRODUCT + "\n" + d.OBS_VALUE + " TJ/a";});
 
-            //axes
-            xAxisEl.transition()
-                .duration(duration)
-                .ease(ease)
-                .call(xAxis)
-                .attr("transform", "translate(0," + yScale(0) + ")");
+            xScale.domain(xDomain);
+            yScale.domain(yDomain);
 
+            //http://bl.ocks.org/enjalot/1429426   has good example on transitions
+
+            //2 transitions: new scale on Y, and new scale on X.
             yAxisEl.transition()
                 .duration(duration)
                 .ease(ease)
                 .call(yAxis);
-
-            /*var duration = 1000
-            arcs = arcs.data(pie(data))
-            arcs.transition()
+            xAxisEl.transition()
                 .duration(duration)
-                .attrTween('d', arcTween)
-
-            arcs.enter()
-                .append('path')
-                .style('stroke', 'white')
-                .attr('fill', function (d, i) {return color(i)})
-                .each(function (d) {this._current = { startAngle: 2 * Math.PI - 0.001, endAngle: 2 * Math.PI }})
-                .transition()
+                .ease(ease)
+                .attr("transform", "translate(0," + yScale(0) + ")")
+                .call(xAxis);
+            bars.exit().transition()
                 .duration(duration)
-                .attrTween('d', arcTween)
-
-            arcs.exit()
-                .transition()
+                .ease(ease)
+                .attr("x", rect.left)
+                .attr("y", rect.top)
+                .attr("height", rect.height)
+                .attr("width", rect.width)
+                .style("opacity", 0)
+                .remove();
+            bars.transition()
                 .duration(duration)
-                .each(function (d) {
-                    d.startAngle = 2 * Math.PI - 0.001;
-                    d.endAngle = 2 * Math.PI;
-                })
-                .attrTween('d', arcTween).remove();//*/
-
+                .ease(ease)
+                //.attr("class", function(d){var cl = "bar pr" + d.PRODUCT; if (d.OBS_VALUE < 0) cl += " neg"; return cl;}) //'classed' not working in transition for some reason
+                .attr("x", rect.left)
+                .attr("y", rect.top)
+                .attr("height", rect.height)
+                .attr("width", rect.width)
+                .style("opacity", 1);
 
         });
 
@@ -259,3 +215,18 @@ function showError (error) {
 }
 
 //TODO: })();
+
+//Find unique property values in array of objects.
+function uniqueValues(array, propName) {
+    var found = {},
+        output = [],
+        l = array.length,
+        propVal;
+    for (var i = 0; i < l; i++) {
+        propVal = array[i][propName];
+        if (found[propVal]) continue;
+        found[propVal] = true;
+        output.push(propVal);
+    }
+    return output;
+}
